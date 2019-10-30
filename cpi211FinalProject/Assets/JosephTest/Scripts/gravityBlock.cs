ea17a65a6d00bf16ddf23093dc7390649a5df9f1
joseph <josephuang0327@gmail.com>
2019-10-30 16:09:58 -0700

    edit script

--- cpi211FinalProject/Assets/JosephTest/Scripts/gravityBlock.cs
+++ cpi211FinalProject/Assets/JosephTest/Scripts/gravityBlock.cs
@@ -5,7 +5,14 @@ using UnityEngine;
 public class gravityBlock : MonoBehaviour
 {
     //public float gravitySpeed = 15f;
+    public float movementSpeed = 300f;
+    public GameObject self;
 
+    bool up = false;
+    bool down = false;
+    bool left = false;
+    bool right = false;
+    bool terrain = false;
 
     void Start()
     {
@@ -16,30 +23,47 @@ public class gravityBlock : MonoBehaviour
     void Update()
     {
 
-        if (Input.GetKeyDown("w"))
-        {
-            Physics.gravity = new Vector3(0, 15f, 0);
-        }
-        if (Input.GetKeyDown("a"))
-        {
-            Physics.gravity = new Vector3(-15f, 0, 0);
-        }
-        if (Input.GetKeyDown("s"))
-        {
-            Physics.gravity = new Vector3(0, -15f, 0);
-        }
-        if (Input.GetKeyDown("d"))
-        {
-            Physics.gravity = new Vector3(15f, 0, 0);
-        }
+        if (up == false && down == false && left == false && right == false)
 
+            if (Input.GetKey("w"))  //move up
+            {
+                up = true;
+                self.GetComponent<Rigidbody>().AddForce(self.transform.up * movementSpeed);
+            }
+            if (Input.GetKey("a"))  //move left
+            {
+                left = true;
+                self.GetComponent<Rigidbody>().AddForce(-self.transform.forward * movementSpeed);
+            }
+            if (Input.GetKey("s")) //move down
+            {
+                down = true;
+                self.GetComponent<Rigidbody>().AddForce(-self.transform.up * movementSpeed);
+            }
+            if (Input.GetKey("d"))  //move right
+            {
+                right = true;
+                self.GetComponent<Rigidbody>().AddForce(self.transform.forward * movementSpeed);
+            }
+        
     }
     private void OnTriggerEnter(Collider collision)
     {
         if (collision.gameObject.tag == "wall")
         {
+            up = false;
+            down = false;
+            left = false;
+            right = false;
+
+            self.GetComponent<Rigidbody>().AddForce(self.transform.forward * 0);
+
             Debug.Log("hit wall");
 
+
+        }
+        if (collision.gameObject.tag == "terrain")
+        {
 
         }
     }
@@ -63,5 +87,23 @@ public class gravityBlock : MonoBehaviour
            transform.position += transform.forward * Time.deltaTime * movementSpeed;
        }
        */
+    /*
+     if (Input.GetKeyDown("w"))
+     {
+         Physics.gravity = new Vector3(0, 15f, 0);
+     }
+     if (Input.GetKeyDown("a"))
+     {
+         Physics.gravity = new Vector3(-15f, 0, 0);
+     }
+     if (Input.GetKeyDown("s"))
+     {
+         Physics.gravity = new Vector3(0, -15f, 0);
+     }
+     if (Input.GetKeyDown("d"))
+     {
+         Physics.gravity = new Vector3(15f, 0, 0);
+     }
+     */
 
 }
