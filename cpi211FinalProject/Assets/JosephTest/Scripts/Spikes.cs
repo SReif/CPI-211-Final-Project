7b49755c7b8392fade1442a0392c116c538a994b
joseph <josephuang0327@gmail.com>
2019-10-29 23:18:47 -0700

    spikes script unfinish~

---
+++ cpi211FinalProject/Assets/JosephTest/Scripts/Spikes.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Spikes : MonoBehaviour
+{
+    public Slider healthbar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.GetComponent<HealthSystem>().Damage(10);
+        }
+    }
+}
