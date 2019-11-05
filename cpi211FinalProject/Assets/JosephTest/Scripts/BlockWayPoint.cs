20d0bc797a742dd60740c07db0eb911ff48f1a8b
joseph <josephuang0327@gmail.com>
2019-11-05 16:11:35 -0700

    new script blockWayPoint

---
+++ cpi211FinalProject/Assets/JosephTest/Scripts/BlockWayPoint.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockWayPoint : MonoBehaviour
+{
+    public Transform self;
+    public float movementSpeed = 100f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.up * Time.deltaTime * movementSpeed;
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == "wall")
+        {
+            movementSpeed = -movementSpeed;
+        }
+    }
+}
+
