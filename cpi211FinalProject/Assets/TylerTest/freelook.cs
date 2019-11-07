f93a75b40be438a500d7d72519a530d010d832e2
TylerONE <yorukagekitsune@gmail.com>
2019-11-06 19:25:31 -0700

    dual camera setup

---
+++ cpi211FinalProject/Assets/TylerTest/freelook.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+public class freelook : MonoBehaviour
+{
+    public CinemachineFreeLook free;
+    public CinemachineVirtualCamera follower;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            free.Priority = 99;
+        }
+        else
+        {
+            free.Priority = 5;
+        }
+    }
+}
