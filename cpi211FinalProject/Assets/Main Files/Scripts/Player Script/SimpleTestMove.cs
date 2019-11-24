6fa09a2140439496234403a3d5b806463192c6ba
JakeHresh <jakeyhresh123@gmail.com>
2019-11-23 17:15:39 -0700

    Modified Sef's test scene to allow player movement while parented

---
+++ cpi211FinalProject/Assets/Main Files/Scripts/Player Script/SimpleTestMove.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleTestMove : MonoBehaviour
+{
+    public float speed = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMovement();
+    }
+
+    void PlayerMovement()
+    {
+        float hor = Input.GetAxis("Horizontal");
+        float ver = Input.GetAxis("Vertical");
+
+        Vector3 playerMovement = new Vector3(hor, 0f, ver) * speed * Time.deltaTime;
+        transform.Translate(playerMovement, Space.Self);
+    }
+}
