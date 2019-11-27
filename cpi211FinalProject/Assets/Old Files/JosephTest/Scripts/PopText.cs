36f4ce5e1a1ce4fe611a80a16532bd82aaad08c1
joseph <josephuang0327@gmail.com>
2019-11-27 15:35:00 -0700

    add pop text script

---
+++ cpi211FinalProject/Assets/Old Files/JosephTest/Scripts/PopText.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopText : MonoBehaviour
+{
+    public Text text;
+    void Start()
+    {
+        Debug.Log("start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("p"))
+        {
+
+                Debug.Log("text disapear");
+                text.enabled = false;
+        }
+        if (Input.GetKeyDown("o"))
+        {
+                Debug.Log("text apear");
+                text.enabled = true;
+        }
+    }
+}
