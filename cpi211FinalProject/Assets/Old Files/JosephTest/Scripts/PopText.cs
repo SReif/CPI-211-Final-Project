b978e75cfc646386cd0d3a538601d632d5f22bef
joseph <josephuang0327@gmail.com>
2019-11-30 20:51:36 -0700

    new update for pop text

--- cpi211FinalProject/Assets/Old Files/JosephTest/Scripts/PopText.cs
+++ cpi211FinalProject/Assets/Old Files/JosephTest/Scripts/PopText.cs
@@ -5,6 +5,8 @@ using UnityEngine.UI;
 
 public class PopText : MonoBehaviour
 {
+    public float timeCount = 2;
+
     public Text bossStage;
     public Text gravity;
     public Text PRotation;
@@ -12,10 +14,10 @@ public class PopText : MonoBehaviour
     public Image textBG;
 
     private bool checkFinalStage = false;
-
+    private bool counting = false;
     void Start()
     {
-        Debug.Log("start");
+       
         gravity.enabled = false;
         bossStage.enabled = false;
         UIboss.enabled = false;
@@ -26,37 +28,59 @@ public class PopText : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        /*if (checkFinalStage == false && bosshealth == 100)      // check boss exist in final stage.
+        
+
+        Debug.Log(timeCount);
+        /*if (checkFinalStage == false && bosshealth == 100)     // check boss exist in final stage.
         {
             exmaple.enabled = true;
             textBG.enabled = true;
             UIboss.enabled = true;
         }
         */
-        if (Input.GetKeyDown("tab"))                               // text for gravity change
+        if (Input.GetKeyDown("tab"))                              // text for gravity change
         {
-            Debug.Log("text disapear");
+            Debug.Log("text appear");
             gravity.enabled = true;
             UIboss.enabled = true;
             textBG.enabled = true;
+
+            counting = true;
+            timeCount = 2;
+
         }
-        if (Input.GetKeyDown("q")|| Input.GetKeyDown("e"))        // text for rotation
+        if (Input.GetKeyDown("q")|| Input.GetKeyDown("e"))       // text for rotation
         {
             PRotation.enabled = true;
             textBG.enabled = true;
             UIboss.enabled = true;
+
+            counting = true;
+            timeCount = 2;
         }
 
 
-
-            if (Input.GetKeyDown("p"))
+        if (counting == true)                                    // counting down
         {
-            Debug.Log("text apear");
-            bossStage.enabled = false;
-            gravity.enabled = false;
-            UIboss.enabled = false;
-            textBG.enabled = false;
-            PRotation.enabled = false;
+            timeCount -= Time.deltaTime;
         }
+
+        if (timeCount <= 0)                                     // time = 0, text disappear
+        {
+            textDisappear();
+        }
+
+
+    }
+
+    void textDisappear()                                        // text disappear function
+    {
+        Debug.Log("text disappear");
+        bossStage.enabled = false;
+        gravity.enabled = false;
+        UIboss.enabled = false;
+        textBG.enabled = false;
+        PRotation.enabled = false;
+        counting = false;
     }
 }
