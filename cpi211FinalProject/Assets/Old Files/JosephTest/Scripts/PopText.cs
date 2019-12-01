7c2ae428d7d26b2465e764cafc9aabd3fcf9055c
joseph <josephuang0327@gmail.com>
2019-11-30 20:19:48 -0700

    update pop text

--- cpi211FinalProject/Assets/Old Files/JosephTest/Scripts/PopText.cs
+++ cpi211FinalProject/Assets/Old Files/JosephTest/Scripts/PopText.cs
@@ -5,25 +5,58 @@ using UnityEngine.UI;
 
 public class PopText : MonoBehaviour
 {
-    public Text text;
+    public Text bossStage;
+    public Text gravity;
+    public Text PRotation;
+    public Image UIboss;
+    public Image textBG;
+
+    private bool checkFinalStage = false;
+
     void Start()
     {
         Debug.Log("start");
+        gravity.enabled = false;
+        bossStage.enabled = false;
+        UIboss.enabled = false;
+        textBG.enabled = false;
+        PRotation.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown("p"))
+        /*if (checkFinalStage == false && bosshealth == 100)      // check boss exist in final stage.
         {
-
-                Debug.Log("text disapear");
-                text.enabled = false;
+            exmaple.enabled = true;
+            textBG.enabled = true;
+            UIboss.enabled = true;
         }
-        if (Input.GetKeyDown("o"))
+        */
+        if (Input.GetKeyDown("tab"))                               // text for gravity change
         {
-                Debug.Log("text apear");
-                text.enabled = true;
+            Debug.Log("text disapear");
+            gravity.enabled = false;
+            UIboss.enabled = false;
+            textBG.enabled = false;
+        }
+        if (Input.GetKeyDown("q")|| Input.GetKeyDown("e"))        // text for rotation
+        {
+            PRotation.enabled = true;
+            textBG.enabled = true;
+            UIboss.enabled = true;
+        }
+
+
+
+            if (Input.GetKeyDown("p"))
+        {
+            Debug.Log("text apear");
+            bossStage.enabled = true;
+            gravity.enabled = true;
+            UIboss.enabled = true;
+            textBG.enabled = true;
+            PRotation.enabled = true;
         }
     }
 }
